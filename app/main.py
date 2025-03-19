85508dea731323a95f774b33a1a5b4c2d3c2752f
Anton Yakovenko <yakovenkoanton2007@gmail.com>
2025-03-19 01:45:10 +0100

    Updating

--- app/main.py
+++ app/main.py
@@ -1,38 +1,37 @@
 class Car:
-    def __init__(self, comfort: int, clean : int, brand: str):
-        self.comfort = comfort
-        self.clean = clean
+    def __init__(self, comfort_class: int, clean_mark : int, brand: str):
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_center: int, power: int,
-                 avr_rating: int, count_of_ratings: int):
-        self.distance_center = distance_center
-        self.power = power
-        self.avr_rating = avr_rating
+    def __init__(self, distance_from_city_center: int, clean_power: int,
+                 average_rating: int, count_of_ratings: int):
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
     def serve_cars(self, cars: list) -> int:
         income = 0
         for car in cars:
             income += self.wash_single_car(car)
-        return income
+        return round(income, 1)
 
     def calculate_washing_price(self, auto: Car) -> float:
-        return round(auto.comfort * (self.power - auto.clean)
-                     * (self.avr_rating / self.distance_center),
-                     1)
+        return auto.comfort_class * (self.clean_power - auto.clean_mark) \
+            * (self.average_rating / self.distance_from_city_center)
 
     def wash_single_car(self, car: Car) -> float:
-        if car.clean < self.power:
+        if car.clean_mark < self.clean_power:
             price = self.calculate_washing_price(car)
-            car.clean = self.power
+            car.clean_mark = self.clean_power
             return price
         return 0
 
     def rate_service(self, mark: int) -> None:
-        self.avr_rating = round((self.avr_rating
-                                * self.count_of_ratings + mark)
-                                / (self.count_of_ratings + 1), 1)
+        self.average_rating = round((self.average_rating
+                                     * self.count_of_ratings + mark)
+                                    / (self.count_of_ratings + 1), 1)
         self.count_of_ratings += 1
